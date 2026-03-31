2dbb067fa22080f57b1d2d8199fd794b791c836d
Sathya Nikhilesh <90498125+snikhilesh8811@users.noreply.github.com>
2026-03-31 08:17:22 +0530

    Skill Experiment 8

---
+++ git-skill-8/src/main/java/com/ecommerce/productsearch/model/Product.java
@@ -0,0 +1,34 @@
+package com.ecommerce.productsearch.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private double price;
+
+    public Product() {}
+
+    public Product(Long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public double getPrice() { return price; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setCategory(String category) { this.category = category; }
+    public void setPrice(double price) { this.price = price; }
+}
